b19896c46bff2cfb5267e0b250d3fbe18b70caa6
Nikolaj Goodger <Nikolaj.Goodger@teaam.telstra.com>
2017-10-04 07:39:54 +1100

    Added question 3.

--- easy21_table.py
+++ easy21_table.py
@@ -2,14 +2,23 @@ import numpy as np
 from numba import njit
 
 
+# Easy 21 Value function table
 def create_v():
     v = np.zeros((10, 21))
     return v
 
 
+# Easy 21 Q Value function table
 def create_q():
     q = np.zeros((10, 21, 2))
     return q
+
+
+# Easy 21 Eligibility trace table
+@njit
+def create_e():
+    e = np.zeros((10, 21, 2))
+    return e
 
 
 @njit
@@ -17,11 +26,16 @@ def set_v(v, dealer_score, player_score, value):
     v[dealer_score - 1, player_score - 1] = value
     return v
 
-
 @njit
 def set_q(q, dealer_score, player_score, action, qvalue):
     q[dealer_score - 1, player_score - 1, action] = qvalue
     return q
+
+
+@njit
+def increment_e(e, dealer_score, player_score, action):
+    e[dealer_score - 1, player_score - 1, action] += 1
+    return e
 
 
 @njit
@@ -32,3 +46,26 @@ def get_v(v, dealer_score, player_score):
 @njit
 def get_q(q, dealer_score, player_score, action):
     return q[dealer_score - 1, player_score - 1, action]
+
+
+@njit
+def get_e(e, dealer_score, player_score, action):
+    return e[dealer_score - 1, player_score - 1, action]
+
+
+@njit
+def decay_e(e, discount_factor, sarsa_lambda):
+    return e * discount_factor * sarsa_lambda
+
+
+@njit
+def decay_state_e(e, dealer_score, player_score, action, discount_factor,
+                  sarsa_lambda):
+    e[dealer_score - 1, player_score - 1, action] *= (discount_factor *
+                                                      sarsa_lambda)
+    return e
+
+
+@njit
+def reset_e(e):
+    return e * 0.0
